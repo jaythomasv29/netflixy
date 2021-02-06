f78f9b0808fe73c27bc871c4dffb0ef792300aeb
James Vongampai <jaythomasv29@gmail.com>
2021-02-06 11:50:53 -0800

    add banner component to display main movie for user with proper css

--- src/Banner.js
+++ src/Banner.js
@@ -15,21 +15,47 @@ function Banner() {
         Math.random() * request.data.results.length
       );
       setMovie(request.data.results[randomPosition]); // random movie set as state
-
       return request;
     }
     fetchData();
   }, []);
 
   console.log(movie);
+  // A function to truncate long movie description text
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
   return (
-    <header>
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+          "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: "center center",
+      }}
+    >
       {/** Background image to cover header */}
 
       {/** title */}
-      {/** div > 2 buttons for Play and My List */}
       {/**Description paragraph */}
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        {/** div > 2 buttons for Play and My List */}
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 270)}
+        </h1>
+      </div>
+      <div className="banner__fadeBottom"></div>
     </header>
   );
 }
